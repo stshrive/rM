7cee197962b0e09456710db278aeba6bb7bc2617
stshrive <aether@LAPTOP-V20OHTP9.localdomain>
2018-04-21 02:01:14 -0400

    split main out to separate file

--- doc_normalize.py
+++ doc_normalize.py
@@ -1,53 +1,8 @@
-import io
 import os
 import re
-import sys
-
-import argparse
-import platform
-
-import doc_rename
 
 year_regex = re.compile(r'/[0-9]{2}/[0-9]{2}/')
 dir_names  = set()
-
-def main(path, extensions):
-    files = get_files(path, extensions)
-
-    for f in files:
-        new_name = correct_name(f)
-        print('{old}\n->  {new}'.format(old=f, new=new_name))
-        doc_rename.safe_rename(f, new_name)
-
-def cleanup():
-    for d in dir_names:
-        parent = os.path.dirname(d)
-        if len(os.listdir(d)) == 0:
-            os.rmdir(d)
-
-        while len(os.listdir(parent)) == 0:
-            os.rmdir(parent)
-            parent = os.path.dirname(parent)
-
-def validate_args(args):
-    ext = [args.pdf, args.txt, args.doc, args.gdoc]
-    if not any(ext) and not args.all:
-        raise
-
-    if args.all and any(ext):
-        raise
-
-def prepare_args(args):
-    ext = [args.pdf, args.txt, args.doc, args.gdoc]
-
-    if args.all:
-        extensions_str = '(txt|doc|docx|gdoc|pdf)'
-    else:
-        extensions_str = '({})'.format('|'.join(list(filter(None,[
-            'pdf'if ext[0] else None, 'txt' if ext[1] else None,
-            'doc' if ext[2] else None,'docx'if ext[2] else None,'gdoc'if ext[3] else None]))))
-
-    return args.directory, extensions_str
     
 def get_files(path, extensions):
     walk_data = os.walk(path)
@@ -86,36 +41,4 @@ def get_name(path):
     ext = path.rfind('.')
     return path[path.rfind('/') + 1:ext], path[ext:]
 
-def get_normalized_name(name):
-    name = name.lower()
-    for c in ['-', '_', '[', ']']:
-        if c in name:
-            name = name.replace(c, ' ')
-
-    name = name.strip()
-    name = '_'.join(name.split())
-    return name
-
-def rename(old, new):
-    os.rename(old, new)
-
-
-if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(sys.argv[0])
-
-    argparser.add_argument('-d', '--directory', type=str, default='.')
-    argparser.add_argument('-R', '--remove-empty', action='store_true', default=False)
-    argparser.add_argument('-P', '--pdf',  action='store_true', default=False)
-    argparser.add_argument('-T', '--txt',  action='store_true', default=False)
-    argparser.add_argument('-D', '--doc',  action='store_true', default=False)
-    argparser.add_argument('-G', '--gdoc', action='store_true', default=False)
-
-    argparser.add_argument('--all', action='store_true')
-
-    args = argparser.parse_args()
-    validate_args(args)
-    main(*prepare_args(args))
-
-    if args.remove_empty:
-        cleanup()
 
